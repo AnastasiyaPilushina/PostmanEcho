65e5c477d364fe3e5d76fd7321aec8252fbc16ea
AnastasiyaPilushina <pilushina22@gmail.com>
2021-04-03 16:16:25 +0300

    First commit

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,36 @@
+package ru.netology;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldPostData() {
+        // Given - When - Then
+       // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+        // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+                .contentType(ContentType.JSON);
+
+
+
+
+
+    }
+
+
+}
+
